34be2d2d86e48e94ce0e24c9b4bae7eebbf395a6
ArtificialCoder-02 <rc.tuhin02@gmail.com>
2024-03-14 00:53:06 +0530

    Completed basic dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,79 @@
-const DashboardPage = () =>{
-    return(
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+
+const tools=[
+    {
+        label:"Conversation",
+        icon : MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href : "/conversation"
+    },
+    {
+        label:"Music Generation",
+        icon : Music,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href : "/music"
+    },
+    {
+        label:"Image Generation",
+        icon : ImageIcon,
+        color: "text-orange-500",
+        bgColor: "bg-orange-500/10",
+        href : "/image"
+    },
+    {
+        label:"Video Generation",
+        icon : VideoIcon,
+        color: "text-yellow-500",
+        bgColor: "bg-yellow-500/10",
+        href : "/video"
+    },
+    {
+        label:"Code Generation",
+        icon : Code,
+        color: "text-indigo-500",
+        bgColor: "bg-indigo-500/10",
+        href : "/code"
+    }
+]
+
+
+const DashboardPage = () => {
+    const router = useRouter();
+    return (
         <div>
-        <p>Dashboard Page (Protected)</p>
+          <div className="mb-8 space-y-4">
+            <h2 className="text-2xl md:text-4xl font-bold text-center">
+              Explore the power of Generative-AI
+            </h2>
+            <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+              All-in-1 Solutions - Chat with Pragya!
+            </p>
+          </div>
+          <div className="px-4 md:px-20 lg:px-32 space-y-4">
+            {tools.map((tool) => (
+              <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-8 h-8", tool.color)} />
+                  </div>
+                  <div className="font-semibold">
+                    {tool.label}
+                  </div>
+                </div>
+                <ArrowRight className="w-5 h-5" />
+              </Card>
+            ))}
+          </div>
         </div>
-    )
-}
+      );
+    }
 
 export default DashboardPage;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import { Montserrat } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -53,6 +54,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathaname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -75,6 +77,8 @@ const Sidebar = () => {
               key={route.href}
               className={cn(
                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathaname == route.href ? "text-white bg-white/10":
+                "text-zinc-400"
               )}
             >
               <div className="flex items-center flex-1">
